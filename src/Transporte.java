7b4690eaf3c26906036dc6edf0d7c99b06688832
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-10-21 18:44:18 -0300

    Feat: criado as variaveis da classe Pai, e método para definir as variaveis

---
+++ src/Transporte.java
@@ -0,0 +1,12 @@
+public class Transporte {
+    private String fabricante;
+    private String tipo;
+    private int capacidade;
+
+    public Transporte(String fabricante, String tipo, int capacidade) {
+        this.fabricante = fabricante;
+        this.tipo = tipo;
+        this.capacidade = capacidade;
+    }
+
+}
